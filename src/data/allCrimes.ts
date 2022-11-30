717dd2996787e41b24812049cbd6d58b7512bf73
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2022-11-30 00:37:33 -0300

    feat: create list of all crimes

---
+++ src/data/allCrimes.ts
@@ -0,0 +1,11 @@
+import { Crime } from '../interfaces/crime';
+
+export const allCrimes: Crime[] = [
+    {
+        crime: 'ATROPELAMENTO',
+        type: 'TRÂNSITO',
+        months: 40,
+        trafficTicket: 30.000,
+        haveBail: false,
+    }
+];
