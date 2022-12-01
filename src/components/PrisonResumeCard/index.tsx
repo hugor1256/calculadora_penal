f18d9d34621d8d8e98405a89dd152cf3364636f7
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2022-11-30 22:12:29 -0300

    feat: add export data

--- src/components/PrisonResumeCard/index.tsx
+++ src/components/PrisonResumeCard/index.tsx
@@ -1,15 +1,23 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-import { Crime } from "../../interfaces/crime";
+import { Crime } from '../../interfaces/crime';
+
+import { applyDiscount } from '../../helper/applyDiscount';
 
 import './styles.css';
-import {applyDiscount} from "../../helper/applyDiscount";
 
 interface FormProps {
     crimes: Crime[]
@@ -20,6 +28,23 @@ const PrisonResumeCard = ({
     crimes,
     isFisrtOffender
 }: FormProps) => {
+    const [dioalogOpen, setDialogOpen] = useState<boolean>(false);
+    const [prisonersName, setPrisonersName] = useState<string>('');
+    const [prisonersPassaport, setPrisonersPassaport] = useState<string>('');
+
+    const handleExportPrisonersData = async () => {
+        const textToDiscord = `
+            Prisioneiro: ${prisonersName},
+            Passaporte: ${prisonersPassaport},
+            Total da pena: ${resolveMonths()},
+            Total de multa: ${resolveTrafficTicket().toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})},
+            Prisão feita por:
+            Policiais envolvidos:
+        `;
+
+        await navigator.clipboard.writeText(textToDiscord);
+    }
+
     const resolveTrafficTicket = (): number => {
         const initalTrafficTicket = 20000;
         const totalTrafficTicket = crimes.reduce((acumulador, numero) => acumulador + numero.trafficTicket, 0);
@@ -39,27 +64,70 @@ const PrisonResumeCard = ({
     }
 
     return (
-        <Box className="featherDescriptionCard" sx={{ minWidth: 275 }}>
-            <Card variant="outlined">
-                <CardContent>
-                    <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-                        Resumo da prisão
-                    </Typography>
+        <>
+            <Box className="featherDescriptionCard" sx={{ minWidth: 275 }}>
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                            Resumo da prisão
+                        </Typography>
 
-                    <Typography variant="h5" component="div">
-                        Tempo total da pena: {resolveMonths()} meses
-                    </Typography>
+                        <Typography variant="h5" component="div">
+                            Tempo total da pena: {resolveMonths()} meses
+                        </Typography>
 
-                    <Typography variant="h5">
-                        Total de multa: {resolveTrafficTicket().toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}
-                    </Typography>
-                </CardContent>
+                        <Typography variant="h5">
+                            Total de multa: {resolveTrafficTicket().toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}
+                        </Typography>
+                    </CardContent>
 
-                <CardActions>
-                    <Button size="small">Exportar dados</Button>
-                </CardActions>
-            </Card>
-        </Box>
+                    <CardActions>
+                        <Button onClick={() => setDialogOpen(!dioalogOpen)} size="small">Exportar dados</Button>
+                    </CardActions>
+                </Card>
+            </Box>
+
+            <Dialog open={dioalogOpen}>
+                <DialogTitle>Exportar dados do réu</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Ao cliar em "EXPORTAR DADOS", os dados do réu será copiado para sua
+                        área de transferência. Cole essas informaçoes no e-mail da policia.
+                        Parabéns pela prisão, steve. A prefeitura agradece!
+                    </DialogContentText>
+
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Nome do réu"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={prisonersName}
+                        onChange={e => setPrisonersName(e.target.value)}
+                        required
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="passaport"
+                        label="Passaporte do réu"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={prisonersPassaport}
+                        onChange={e => setPrisonersPassaport(e.target.value)}
+                        required
+                    />
+                </DialogContent>
+
+                <DialogActions>
+                    <Button onClick={() => setDialogOpen(!dioalogOpen)}>Fechar</Button>
+                    <Button disabled={!prisonersName || !prisonersPassaport} onClick={handleExportPrisonersData}>Exportar dados</Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 }
 
