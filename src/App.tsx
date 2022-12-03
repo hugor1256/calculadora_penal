f4d3dae777719fefb00cd0af532469c7db21a1ce
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2022-12-02 22:58:44 -0300

    feat: adjust import

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 import './styles/App.css';
-import LabelBottomNavigation from "./components/Footer";
 
 function App() {
     return (
         <>
             <Header />
             <Main />
-            <LabelBottomNavigation />
+            <Footer />
         </>
     );
 }
